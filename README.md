618fb18ed0f16c27c52cf018f47bb433037062af
scott88lee <scott88lee@gmail.com>
2019-06-03 20:54:18 +0800

    Connecting Express to RDS instance

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Frontend
 VueJS with Semantic UI
 
 EC2 connection:
-ssh -i "tonehouse.pem" ubuntu@ec2-13-250-97-27.ap-southeast-1.compute.amazonaws.com
+ssh -i "tonehouse.pem" ubuntu@ec2-54-255-207-189.ap-southeast-1.compute.amazonaws.com
 
 RDS (PSQL) : Endpoint
 booking.cnwibdw0nva7.ap-southeast-1.rds.amazonaws.com
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const pg = require('pg');
 const app = express();
 
-app.get('*', (req, res) => {
-    res.send('Hello world');
+const config = {
+  user: 'postgres',
+  password: 'T0nehouse',
+  host: 'booking.cnwibdw0nva7.ap-southeast-1.rds.amazonaws.com',
+  database: 'bookingengine',
+  port: 5432
+};
+
+const pool = new pg.Pool(config);
+
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
+
+app.get('*', (req, res) =>{
+	const queryString = "SELECT * FROM users";
+	pool.query(queryString, (err, result) => {
+    	if (err) {
+    		console.error('Query error:', err.stack);
+   		}
+   		
+    	res.send(result.rows);
+	});
 })
 
-app.listen(3000, ()=> console.log('Listen: 3000'));
+app.listen(80, () => console.log('Listen port: 80'));
