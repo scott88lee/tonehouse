0a902b3c2798b79e1636a71dafb04649c3a4d077
scott88lee <scott88lee@gmail.com>
2019-06-04 19:02:23 +0800

    Created login and signup views

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -19,6 +19,16 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: () => import("./views/Signup.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("./views/Login.vue")
     }
   ]
 });
